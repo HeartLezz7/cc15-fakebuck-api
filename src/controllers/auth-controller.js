39d891322bc55d72ffbd03a1d5d3e6c787ecd6ee
HeartLezz7 <heartchantapath@gmail.com>
2023-09-29 10:17:00 +0700

    finish:login

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,67 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { registerSchema, loginSchema } = require("../validators/auth-validator");
+const prisma = require("../models/prisma");
+const createError = require("../utils/create-error");
+
+exports.register = async (req, res, next) => {
+  try {
+    const { value, error } = registerSchema.validate(req.body);
+    if (error) {
+      error.statusCode = 400;
+      return next(error);
+    }
+    console.log(value);
+    value.password = await bcrypt.hash(value.password, 12);
+    const user = await prisma.user.create({
+      data: value,
+    });
+    const payload = { userId: user.id };
+    const accessToken = jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY || "1q2w3e4r5t6y7u8i9o0p",
+      {
+        expiresIn: process.env.JWT_EXPIRE,
+      }
+    );
+    console.log(accessToken);
+    res.status(201).json({ accessToken });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { value, error } = loginSchema.validate(req.body);
+    console.log(value);
+    if (error) {
+      error.statusCode = 400;
+      return next(error);
+    }
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [{ email: value.emailOrMobile }, { mobile: value.emailOrMobile }],
+      },
+    });
+    if (!user) {
+      return next(createError("invalid credential", 400));
+    }
+    const isMatch = await bcrypt.compare(value.password, user.password);
+    if (!isMatch) {
+      return next(createError("invalid credential", 400));
+    }
+    const payload = { userId: user.id };
+    const accessToken = jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY || "1q2w3e4r5t6y7u8i9o0p",
+      {
+        expiresIn: process.env.JWT_EXPIRE,
+      }
+    );
+    console.log(accessToken);
+    res.status(201).json({ accessToken });
+  } catch (err) {
+    next(err);
+  }
+};
